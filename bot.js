4c26bea01a47f84e8e82acb410a6e74666223315
LebsterFace <44923609+LebsterFace@users.noreply.github.com>
2019-08-15 20:37:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-//TEST!
-
 var reminders,waitDate,message,discordMsg;
 client.on("ready", () => {
   console.log("Bot started!");
@@ -31,10 +29,10 @@ function calcTime(time,scale) {
 }
 
 client.on("message", message => {
-  if (message==".rb") message.channel.send("Version: 1.5.0\nNext Verion Additions:\n` - Repeating Reminders\n - AM/PM Support\n - Colon support\n - Custom Timespans`  ");return;
+  if (message.content==".rb") {message.channel.send("Version: 1.5.1\nNext Verion Additions:\n` - Repeating Reminders\n - AM/PM Support\n - Colon support\n - Custom Timespans`  ");return;}
   if (message.author.bot) return;
   const args = message.content.trim().split(/ +/g);
-  if (args.length>1||message.isMatch(/[^0123456789]/gi)) {
+  if (args.length>1||message.content.match(/[0123456789]/gi)!=null) {
     discordMsg=message;
     var waitDate,
     	message=[],
@@ -54,4 +52,4 @@ client.on("message", message => {
 });
 
 
-client.login(process.env.BOT_TOKEN);
+client.login("NjExMzYxMTM0NjU3NjY3MDc1.XVWzCQ.kZvPpw1F0VnT3CqjRlw8JHvzpvQ");
